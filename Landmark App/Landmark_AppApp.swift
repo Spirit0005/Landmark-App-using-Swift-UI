3d4b407308b63e719d02270542797ad3076a7ca5
Spirit0005 <m_hamzak1997@yahoo.com>
2021-06-17 16:34:28 +0500

    Initial Commit

---
+++ Landmark App/Landmark_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_AppApp.swift
+//  Landmark App
+//
+//  Created by Hamza Khalid on 17/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
